308b113ac41c171eeee4630cffa2673638537759
joaquinsosa00 <joaquin150496@gmail.com>
2026-01-22 09:26:16 -0300

    primer commit repaso JS

---
+++ Js/01.js
@@ -0,0 +1,8 @@
+//Bucle del 1 al 20 - Crea un bucle que imprima los números del 1 al 20.
+
+let renglon = 21;
+
+    while (renglon <= 20) {
+        document.writeln(`Renglon numero ${renglon} </br>`);
+        renglon ++;
+    }
